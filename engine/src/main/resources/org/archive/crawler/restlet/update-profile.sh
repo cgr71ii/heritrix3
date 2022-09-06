7e1a43933b5713b156177d1d79a8923f073b5628
Cristian García Romero <cgr71ii@gmail.com>
2022-09-06 11:28:22 +0200

    Generalize profile-crawler-beans.cxml
    profile-crawler-beans.cxml can be modified to one of the provided
    profiles using update-profile.sh

---
+++ engine/src/main/resources/org/archive/crawler/restlet/update-profile.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROFILES_DIR="${DIR}/profiles"
+PROFILE_TARGET="${DIR}/profile-crawler-beans.cxml"
+NEW_PROFILE="./profiles/$1"
+
+if [[ ! -f "$PROFILE_TARGET" ]]; then
+  >&2 echo "WARNING: there is not current profile actived ($PROFILE_TARGET)"
+fi
+
+ls_profiles()
+{
+  >&2 echo "Available profiles ($(ls ""$PROFILES_DIR"" | wc -l)):"
+  >&2 ls "$PROFILES_DIR"
+}
+
+if [[ "$#" -eq "0" ]]; then
+  >&2 echo "Syntax: $(basename $0) <profile>"
+  >&2 echo ""
+  ls_profiles
+
+  exit 1
+fi
+
+pushd "$DIR" > /dev/null
+
+if [[ ! -f "$NEW_PROFILE" ]]; then
+  >&2 echo "Provided profile does not exist ($NEW_PROFILE)"
+  >&2 echo ""
+  ls_profiles
+
+  exit 1
+fi
+
+ln -sf "$NEW_PROFILE" "$PROFILE_TARGET" \
+  && echo "New profile activated: $PROFILE_TARGET -> $NEW_PROFILE" \
+  || >&2 echo "Couldn't activate new profile"
+
+popd > /dev/null
