336cedbbfe0e8fa5e709c9ac78d8ee55404891b9
Cristian Garcia Romero <cgarcia@eshb82942ds.ikexpress.com>
2023-09-25 19:04:56 +0200

    Experiments data

---
+++ crawling-experiments/run-heritrix-experiments.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+d=$(date +%Y%m%d%H%M%S)
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+config_dir="$1"
+langs="$2"
+output_prefix="$3" # e.g. /home/cgarcia/crawling-puc
+
+if [[ -z "$config_dir" ]]; then
+  >&2 echo "Syntax error"
+  exit 1
+fi
+if [[ -z "$langs" ]]; then
+  langs="en-is en-fi en-mt es-eu"
+fi
+if [[ ! -d "$output_prefix" ]]; then
+  >&2 echo "Output directory does not exist"
+fi
+
+echo "Langs: $langs"
+
+heritrix_dir="$DIR/.."
+
+for l in $(echo "$langs"); do
+  o="$output_prefix/$config_dir/$l"
+
+  if [[ -d "$o" ]]; then
+    >&2 echo "Skip (directory already exists): $o"
+    continue
+  fi
+
+  mkdir -p "$o"
+
+  conf_file="$heritrix_dir/crawling-experiments/crawling-config/$config_dir/${l}.cxml"
+  seed="$heritrix_dir/crawling-experiments/crawling-seeds/${l}.tmx.gz.urls.fixes_and_same_domain.same_authority_sort_uniqc.authorities.shuf.without_domains_from_puc_nor_url2lang_train_and_dev.200_and_not_redirect.out.sort_by_uniq_pairs_of_parallel_docs.seeds_200"
+
+  if [[ ! -f "$conf_file" ]]; then
+    >&2 echo "Skip (conf file not found in heritrix dir): $conf_file"
+    continue
+  fi
+  if [[ ! -f "$seed" ]]; then
+    >&2 echo "Skip (seed file not found in heritrix dir): $seed"
+    continue
+  fi
+
+  o="$o/${d}_${l}"
+  echo "$o"
+
+  $heritrix_dir/scripts/create-individual-jobs-from-template.sh \
+    "https://localhost:8443" \
+    "$conf_file" \
+    "$seed" \
+    "$o" \
+    &> "${o}.script.log" &
+
+#    wait
+done
+
+echo "Waiting..."
+wait
+echo "Done!"
